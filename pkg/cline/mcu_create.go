1946d42e56b2766a5ffdd0f14c1d33a1ac8f21e9
MakerYang <open@wileho.com>
2025-06-16 20:44:36 +0800

    0.0.1

--- pkg/cline/mcu_create.go
+++ pkg/cline/mcu_create.go
@@ -83,9 +83,7 @@ func (m McuCreateModel) View() string {
 
 	switch m.state {
 	case "home":
-		text := fmt.Sprintf("Please select a basic microcontroller\n\n")
-		text += fmt.Sprintf("Microcontroller model:\n\n%s\n"+helpStyle.Render("Press Esc to exit."), m.keyword.View())
-		return text
+		return fmt.Sprintf("Please select a basic microcontroller model:\n\n%s\n"+helpStyle.Render("Press Esc to exit."), m.keyword.View())
 	}
 
 	return ""
