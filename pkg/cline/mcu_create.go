c9fc9a524d5a2d6704cdccda444e995a6824d0dc
MakerYang <open@wileho.com>
2025-06-16 23:34:19 +0800

    0.0.1

--- pkg/cline/mcu_create.go
+++ pkg/cline/mcu_create.go
@@ -124,8 +124,7 @@ func (m McuCreateModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			m.items = list.New(msg.items, list.NewDefaultDelegate(), 0, 0)
 			m.items.Title = "Search Results"
 			m.items.SetShowHelp(false)
-			m.items.SetWidth(100)
-			m.items.SetHeight(10)
+			m.items.SetSize(30, 10)
 			m.state = "items"
 		}
 	}
